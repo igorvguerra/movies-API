cdd6451f45f8ce832677b0d3556ba47c4c752cfe
Igor <igor.vguerra@hotmail.com>
2024-02-28 13:50:38 +0000

    testing all MovieNotesControllers

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -54,19 +54,20 @@ class MovieNotesController {
 
         let notes;
 
-        if(movie_tags) {
+        if(tags) {
             const filterTags = tags.split(",").map(tag => tag.trim());
 
-            notes = await knex("movie_tags").select(["movie_notes.id", "movie_notes.title", "movie_notes.user_id"]).where("movie_notes.user_id", user_id).whereLike("movie_notes.title", `%${title}%`).whereIn("name", filterTags).innerJoin("movie_notes", "movie_notes.id", "tags.note_id").orderBy("movie_notes.title");
+            notes = await knex("movie_tags").select(["movie_notes.id", "movie_notes.title", "movie_notes.user_id"]).where("movie_notes.user_id", user_id).whereLike("movie_notes.title", `%${title}%`).whereIn("name", filterTags).innerJoin("movie_notes", "movie_notes.id", "movie_tags.movie_note_id").orderBy("movie_notes.title");
         
         }else {
-            movie_notes = await knex("movie_notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+            notes = await knex("movie_notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
         }
        
 
         const userTags = await knex("movie_tags").where({ user_id });
+        
         const notesWithTags = notes.map(note => {
-            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+            const noteTags = userTags.filter(tag => tag.movie_note_id === note.id);
 
             return {
                 ...note,
