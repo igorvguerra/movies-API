1942157513ecd8f356c09daa42ea2d3fe7ba7eb6
Igor <igor.vguerra@hotmail.com>
2024-02-28 13:53:56 +0000

    testing MovieTagsControllers

--- src/controllers/MovieTagsController.js
+++ src/controllers/MovieTagsController.js
@@ -4,7 +4,7 @@ class MovieTagsController {
     async index(request, response) {
         const { user_id } = request.params;
 
-        const tags = await knex("tags").where({ user_id })
+        const tags = await knex("movie_tags").where({ user_id })
 
         return response.json(tags);
     }
